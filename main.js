1dbb8cfcfdc0b2c1adeb69e92f37eb01ba45a829
Ouahabi Amine <amine@BECHAR.CASNOS.DZ>
2019-03-14 16:12:39 +0100

    big move

---
+++ main.js
@@ -0,0 +1,6 @@
+var currentValue = "";
+function handleClick(myRadio) {
+    //alert('Old value: ' + currentValue);
+    //alert('New value: ' + myRadio.value);
+    currentValue = myRadio.value;
+}
