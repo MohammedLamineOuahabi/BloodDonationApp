894034061f83b8b0fe91113dbbdb2457cbad45e7
Your Name <you@example.com>
2019-03-16 19:55:10 +0100

    phone num mask

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+/***** phone number mask *****************/
+
+$(window).load(function () {
+    var phones = [{ "mask": "+\\2\\13(*) ##-##-##-##" }, { "mask": "+213(*) ##-##-##-##" }];
+    $('#tel').inputmask({
+        mask: phones,
+        greedy: false,
+        definitions: { '#': { validator: "[0-9]", cardinality: 1 },'*': { validator: "[5-7]", cardinality: 1 } }
+    });
+});
+
+/*****************************************/
 var currentValue = "";
 function handleClick(myRadio) {
     //alert('Old value: ' + currentValue);
